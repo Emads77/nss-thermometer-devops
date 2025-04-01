0cd7ce11946770a229e74ffed14377de4f5a3fc6
tavgar <administrator@gitea.searcher.htb>
2025-04-01 23:43:53 +0200

    auth vars

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
 
 
 resource "aws_vpc" "app_vpc" {
